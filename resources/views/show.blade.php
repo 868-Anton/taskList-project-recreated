067be3e965615cd5a97504e22461b74017160f2f
868Anton <anton.graham.2011@gmail.com>
2024-07-27 19:54:07 -0400

    Styling the Task Page - Tailwind CSS Add Navigation Link to Task List: Style Description and Long Description: Format Creation and Update Dates: Indicate Task Completion Status: Style Action Buttons: Update Styles and Layout:

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,46 +2,53 @@
 
 @section('title', $task->title)
 
-<a href="{{ route('tasks.index') }}">🔙 Go Back to Task List</a>
 
 @section('content')
 
-<p>{{ $task->description }}</p>
+<div class="mb-4 underline">
+  <a href="{{ route('tasks.index') }}">🔙 Go Back to Task List</a>
+</div>
+
+<p class="mb-4">{{ $task->description }}</p>
 
 @if($task->long_description)
-<p>{{ $task->long_description }}</p>
+<p class="mb-4">{{ $task->long_description }}</p>
 @endif
 
 
-<p>{{ $task->created_at }}</p>
-<p>{{ $task->updated_at }}</p>
-
-<p>
+<p class="mb-4">
   @if ($task->completed)
-  Completed
-  @else Not completed
+  <span class="font-medium text-green-500">Completed</span>
+  @else <span class="font-medium text-red-500">Not completed</span>
   @endif
 </p>
 
-<div>
-  <form method="POST" action="{{ route('tasks.destroy', ['task'=>$task->id]) }}">
-    @csrf
-    @method('DELETE')
-    <button type="submit">Delete</button>
-  </form>
-</div>
+<p class="mb-4 text-slate-500 text-sm">
+  Created {{ $task->created_at->diffForHumans() }} · {{ $task->updated_at->diffForHumans() }}
+</p>
 
-<div>
-  <form action="{{ route('tasks.toggle-complete', ['task'=>$task] )}}" method="POST">
-    @csrf
-    @method('PUT')
-    <button type="submit">
-      Mark as {{ $task->completed ? 'not completed' : 'is completed' }}
-    </button>
-  </form>
-</div>
+<div class="flex gap-3">
+  <div class="btn">
+    <a href="{{ route('tasks.edit',['task'=>$task->id]) }}">Edit</a>
+  </div>
 
-<div>
-  <a href="{{ route('tasks.edit',['task'=>$task->id]) }}">Edit</a>
+  <div class="btn">
+    <form method="POST" action="{{ route('tasks.destroy', ['task'=>$task->id]) }}">
+      @csrf
+      @method('DELETE')
+      <button type="submit">Delete</button>
+    </form>
+  </div>
+  
+  <div class="btn">
+    <form action="{{ route('tasks.toggle-complete', ['task'=>$task] )}}" method="POST">
+      @csrf
+      @method('PUT')
+      <button type="submit">
+        Mark as {{ $task->completed ? 'not completed' : 'is completed' }}
+      </button>
+    </form>
+  </div>
+  
 </div>
 @endsection
